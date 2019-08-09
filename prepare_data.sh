97da0e4f65ff7097519d21d8e382f65d57aff1c9
Chonghe <chong.he07@gmail.com>
2019-08-09 11:03:22 +0800

    change dataset dir

--- prepare_data.sh
+++ prepare_data.sh
@@ -2,4 +2,6 @@
 
 mkdir -p raw/train
 wget -O a.tar.gz https://data.mendeley.com/datasets/nz4hy7yrps/1/files/809ae272-5c75-4d32-93a1-31a192ad57db/TVHeads.tar.gz\?dl\=1
-tar zxf a.tar.gz -C raw/train
+tar zxf a.tar.gz
+mv dataset/* raw/train/
+rm -rf a.tar.gz dataset
