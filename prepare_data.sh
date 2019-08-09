98c369c937ae6ee269225d01918a399980c2282d
Chonghe <chong.he07@gmail.com>
2019-08-09 10:34:39 +0800

    add data prepare script

---
+++ prepare_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p raw/train
+wget -O a.tar.gz https://data.mendeley.com/datasets/nz4hy7yrps/1/files/809ae272-5c75-4d32-93a1-31a192ad57db/TVHeads.tar.gz\?dl\=1
+tar zxf a.tar.gz -C raw/train
